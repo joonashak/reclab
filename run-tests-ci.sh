7306984cd2e7b78208edf7bff3e29b7ef19e18b2
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-10-13 21:03:04 +0300

    Add lint check to CI

--- run-tests-ci.sh
+++ run-tests-ci.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 docker-compose -f docker-compose-ci.yml up -d db-ci cms-ci web-ci
+
+# Linting
+docker-compose -f docker-compose-ci.yml exec cms-ci npm run lint
+docker-compose -f docker-compose-ci.yml exec web-ci npm run lint
+
+# E2E
 docker-compose -f docker-compose-ci.yml run e2e-ci || exit 1
 
 # This is not strictly necessary in CI, but helps when debugging things locally, etc.
