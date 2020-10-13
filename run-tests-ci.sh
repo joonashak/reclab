22299d8adb584c0df04ab5402bc0384306100e8e
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-10-13 21:09:46 +0300

    Disable TTY allocation in docker-compose exec

--- run-tests-ci.sh
+++ run-tests-ci.sh
@@ -3,8 +3,8 @@
 docker-compose -f docker-compose-ci.yml up -d db-ci cms-ci web-ci
 
 # Linting
-docker-compose -f docker-compose-ci.yml exec cms-ci npm run lint
-docker-compose -f docker-compose-ci.yml exec web-ci npm run lint
+docker-compose -f docker-compose-ci.yml exec -T cms-ci npm run lint
+docker-compose -f docker-compose-ci.yml exec -T web-ci npm run lint
 
 # E2E
 docker-compose -f docker-compose-ci.yml run e2e-ci || exit 1
