43613d5c7765ae8c3abe98edeed6ec5e4b229f03
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-10-13 20:45:53 +0300

    Clean up

--- e2e/cypress/integration/test.spec.js
+++ e2e/cypress/integration/test.spec.js
@@ -3,7 +3,7 @@
 describe('My First Test', () => {
   it('Does not do much!', () => {
     cy.visit('/');
-    cy.contains('Test Page 999');
+    cy.contains('Test Page 1');
     cy.contains('Test Page 2');
   });
 });
--- run-tests-ci.sh
+++ run-tests-ci.sh
@@ -2,4 +2,6 @@
 
 docker-compose -f docker-compose-ci.yml up -d db-ci cms-ci web-ci
 docker-compose -f docker-compose-ci.yml run e2e-ci || exit 1
+
+# This is not strictly necessary in CI, but helps when debugging things locally, etc.
 docker-compose -f docker-compose-ci.yml down
