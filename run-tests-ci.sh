738b3e4fa9d7e778f9ce9e99322670c8cc40ab2e
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-10-13 19:53:59 +0300

    Maybe I just suck at bash?

--- run-tests-ci.sh
+++ run-tests-ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker-compose -f docker-compose-ci.yml up -d db-ci cms-ci web-ci
-docker-compose -f docker-compose-ci.yml run e2e-ci
+docker-compose -f docker-compose-ci.yml run e2e-ci || exit 1
 docker-compose -f docker-compose-ci.yml down
