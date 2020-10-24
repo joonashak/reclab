c1d3dd7c98542eb8bc9427321dc8722572fa1cc6
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-10-24 20:01:05 +0300

    Add settings seed data

--- cms/src/seeder/seeder.module.ts
+++ cms/src/seeder/seeder.module.ts
@@ -5,9 +5,10 @@ import { SeederController } from './seeder.controller';
 import { Page } from '../pages/page.entity';
 import { User } from '../users/user.entity';
 import { MenuItem } from '../menu/menuItem.entity';
+import { Settings } from '../settings/settings.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Page, User, MenuItem])],
+  imports: [TypeOrmModule.forFeature([Page, User, MenuItem, Settings])],
   providers: [SeederService],
   controllers: [SeederController],
 })
