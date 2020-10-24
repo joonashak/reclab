18e2832a7aa0b413e398a523f17f936f41213754
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-10-24 19:36:25 +0300

    Only use SSL in production mode

--- cms/ormconfig.js
+++ cms/ormconfig.js
@@ -1,6 +1,18 @@
 // This configuration file is used only with TypeORM CLI!
 const { join } = require('path');
 
+const ssl =
+  process.env.NODE_ENV === 'production'
+    ? {
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : {};
+
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
@@ -8,10 +20,5 @@ module.exports = {
   logging: process.env.NODE_ENV === 'development',
   entities: ['src/**/*.entity.ts'],
   migrations: ['migrations/*.ts'],
-  ssl: true,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
+  ...ssl,
 };
