c9234acbe47c4dad4089239dc7385e82e3d8173e
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-11-11 22:12:51 +0200

    Remove unnecessary code causing CI to fail

--- web/src/templates/Page.tsx
+++ web/src/templates/Page.tsx
@@ -3,27 +3,11 @@ import PropTypes, { InferProps } from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { MDXProvider } from '@mdx-js/react';
-import { graphql, useStaticQuery } from 'gatsby';
-import Img from 'gatsby-image';
 import PageWrapper from '../PageWrapper';
 import components from '../components/mdx/components';
 
-const query = graphql`
-  query {
-    file(childImageSharp: {fixed: {originalName: {eq: "meme.jpg"}}}) {
-      relativePath
-      childImageSharp {
-        fixed {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
-  }
-`;
-
 const Page = ({ pageContext }: InferProps<typeof Page.propTypes>) => {
   const { i18n } = useTranslation();
-  const image = useStaticQuery(query);
 
   useEffect(() => {
     i18n.changeLanguage(pageContext.data.language);
@@ -36,9 +20,6 @@ const Page = ({ pageContext }: InferProps<typeof Page.propTypes>) => {
           {pageContext.data.childMdx.body}
         </MDXRenderer>
       </MDXProvider>
-      {image.file && (
-        <Img fixed={image.file.childImageSharp.fixed} />
-      )}
     </PageWrapper>
   );
 };
