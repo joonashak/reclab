7a2618421339ad69bd6e040627df5dc33c38fdc6
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-11-24 19:38:43 +0200

    Get more images from Cloudinary

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -81,6 +81,7 @@ const imagePlugins = [
       cloudName: process.env.CLOUDINARY_CLOUD_NAME,
       apiKey: process.env.CLOUDINARY_API_KEY,
       apiSecret: process.env.CLOUDINARY_API_SECRET,
+      maxResults: 1000,
     },
   },
   {
