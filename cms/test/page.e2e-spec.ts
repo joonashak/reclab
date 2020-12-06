55a387b513c8ffe80be28beb46caae30c655f96a
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-12-06 13:48:46 +0200

    Add test for POST /page

--- cms/test/page.e2e-spec.ts
+++ cms/test/page.e2e-spec.ts
@@ -14,7 +14,48 @@ describe('/page', () => {
     const res = await request(app.getHttpServer()).get('/page');
 
     expect(res.status).toBe(200);
-    expect(res.body).toEqual(expect.arrayContaining(apiPages.filter(p => p.isPublic)));
+    expect(res.body).toEqual(
+      expect.arrayContaining(apiPages.filter(p => p.isPublic)),
+    );
+  });
+
+  it('/ (POST)', async () => {
+    const authResult = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ username: 'admin', password: '1234' });
+
+    const { accessToken } = authResult.body;
+
+    const testPage = {
+      title: 'Test Page',
+      content: 'Lorem ipsum...',
+      language: 'en',
+      isPublic: true,
+      path: '/test',
+      translationIds: [],
+    };
+
+    const postResult = await request(app.getHttpServer())
+      .post('/page')
+      .send(testPage)
+      .set('Authorization', `Bearer ${accessToken}`);
+
+    expect(postResult.status).toBe(201);
+
+    const { id, createdAt, ...testablePage } = postResult.body;
+
+    expect(testablePage).toEqual({
+      title: 'Test Page',
+      content: 'Lorem ipsum...',
+      language: 'en',
+      isPublic: true,
+      path: '/test',
+      author: {
+        id: 'adc86188-4c93-4993-864e-91a6c129ff8a',
+      },
+      translations: [],
+      updatedAt: null,
+    });
   });
 
   afterEach(async () => {
