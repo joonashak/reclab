d57fe279ceb1bfc5ab67a71bc23d1998299608ad
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-12-11 00:21:46 +0200

    Add PATCH /page

--- cms/src/pages/pages.service.ts
+++ cms/src/pages/pages.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, Connection } from 'typeorm';
 import { Page } from './page.entity';
 import { CreatePageDto } from './dto/create-page.dto';
 
@@ -9,6 +9,7 @@ export class PagesService {
   constructor(
     @InjectRepository(Page)
     private readonly pagesRepository: Repository<Page>,
+    private connection: Connection,
   ) {}
 
   async findOne(id: string): Promise<Page> {
@@ -33,8 +34,6 @@ export class PagesService {
   }
 
   async findAllPublic(): Promise<Page[]> {
-    // Jos SO ei vastaa, niin voi ratkaista tallentamalla relaation molempiin suuntiin luontivaiheessa.
-    // https://stackoverflow.com/questions/64124414/join-self-referencing-relation-both-ways-with-typeorm
     return this.pagesRepository
       .createQueryBuilder('page')
       .leftJoinAndSelect('page.translations', 'translations')
@@ -45,6 +44,13 @@ export class PagesService {
 
   async create(page: CreatePageDto): Promise<Page> {
     const { translationIds, ...newPage } = page;
+
+    // Disallow overwriting existing id.
+    const pageExists = !!(await this.findOne(newPage.id));
+
+    if (pageExists) {
+      throw new HttpException('Page ID exists.', 403);
+    }
 
     // Load translations.
     const translations = await this.findMany(translationIds);
@@ -60,6 +66,56 @@ export class PagesService {
       strippedPage.translations = [];
       translation.translations.push(strippedPage);
       await this.pagesRepository.save(translation);
+    });
+
+    return result;
+  }
+
+  async update(page: CreatePageDto): Promise<any> {
+    const { id, translationIds, ...newPage } = page;
+
+    newPage.updatedAt = new Date();
+
+    // Some fields cannot be changed.
+    delete newPage.author;
+    delete newPage.createdAt;
+
+    const result = await this.pagesRepository
+      .createQueryBuilder('page')
+      .update(Page)
+      .set({ ...newPage })
+      .where({ id })
+      .execute();
+
+    // KISS: Remove all old translations and add current ones, even if same.
+    await this.connection.query(
+      `
+      DELETE FROM translation
+      WHERE "originPageId" = $1 OR "targetPageId" = $1
+    `,
+      [id],
+    );
+
+    translationIds.forEach(async translationId => {
+      // Remove possible existing translations.
+      // TODO: Needs to be filtered by language if this is to handle more than two languages.
+      await this.connection.query(
+        `
+        DELETE FROM translation
+        WHERE "originPageId" = $1 OR "targetPageId" = $1
+      `,
+        [translationId],
+      );
+
+      await this.connection.query(
+        `
+      INSERT INTO translation ("originPageId", "targetPageId")
+      VALUES
+        ($1, $2),
+        ($2, $1)
+    `,
+        [id, translationId],
+      );
     });
 
     return result;
