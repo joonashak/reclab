39b5a886e44d32bdcca008d79ee11c587ae7c88c
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-12-14 19:30:00 +0200

    Mount deployment endpoints only if VERCEL_TOKEN is set

--- cms/src/deployment/deployment.module.ts
+++ cms/src/deployment/deployment.module.ts
@@ -1,10 +1,12 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { DeploymentController } from './deployment.controller';
 
+// Mount the endpoints only if VERCEL_TOKEN is set.
 const VERCEL_TOKEN = process.env.VERCEL_TOKEN || '';
+const controllers = VERCEL_TOKEN ? [DeploymentController] : [];
 
 @Module({
-  controllers: [DeploymentController],
+  controllers,
   imports: [
     HttpModule.register({
       headers: { Authorization: `Bearer ${VERCEL_TOKEN}` },
