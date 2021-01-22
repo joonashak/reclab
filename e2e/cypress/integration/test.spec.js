7a32702b442e78e9d4cdc7afc3828ba96ec88244
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-01-22 21:33:17 +0200

    Add cypress commands

--- e2e/cypress/integration/test.spec.js
+++ e2e/cypress/integration/test.spec.js
@@ -1,6 +1,11 @@
 /// <reference types="Cypress" />
 
 describe("My First Test", () => {
+  it("Test custom commands", () => {
+    cy.init();
+    cy.login();
+  })
+
   it("Does not do much!", () => {
     cy.visit("/");
     cy.contains("Here the freedom is yours.");
