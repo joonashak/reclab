918e47dd3fa6e8562688aaa989bd6d45ab4d1da3
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-01-23 19:54:41 +0200

    Refactor

--- e2e/cypress/integration/admin/menu.spec.ts
+++ e2e/cypress/integration/admin/menu.spec.ts
@@ -1,14 +1,14 @@
+const checkMenuItem = (label: string, pathname: string) => {
+  cy.visit('/admin');
+  cy.cs('hamburger').click();
+  cy.contains(label).click();
+  cy.location('pathname').should('eq', pathname);
+};
+
 describe('Admin menu', () => {
   before(() => {
     cy.login();
   });
-
-  const checkMenuItem = (label: string, pathname: string) => {
-    cy.visit('/admin');
-    cy.cs('hamburger').click();
-    cy.contains(label).click();
-    cy.location('pathname').should('eq', pathname);
-  };
 
   it('Dashboard', () => {
     checkMenuItem('Dashboard', '/admin');
