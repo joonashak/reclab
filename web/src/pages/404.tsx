81d629451037c9af50e6db371dd2983bf36af656
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-01-24 17:15:43 +0200

    Add very simple 404 page

---
+++ web/src/pages/404.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import LayoutWide from '../components/LayoutWide';
+import Headings from '../components/mdx/Headings';
+
+export default () => (
+  <LayoutWide>
+    <Headings.H2>Not found!</Headings.H2>
+  </LayoutWide>
+);
