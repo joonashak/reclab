49bfb13c722ce086a55b818c0973105cbd3ad618
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-01-28 21:32:07 +0200

    Add description and keywords to UI

--- web/src/components/Admin/Pages/PageForm.tsx
+++ web/src/components/Admin/Pages/PageForm.tsx
@@ -40,6 +40,22 @@ const PageForm = ({
         />
       </Grid>
       <Grid item xs={12} md={6}>
+        <ControlledTextField
+          formControl={formControl}
+          name="description"
+          label="Description"
+          rules={{ required: 'Description cannot be empty' }}
+        />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <ControlledTextField
+          formControl={formControl}
+          name="keywords"
+          label="Keywords"
+          rules={{ required: 'Keywords cannot be empty' }}
+        />
+      </Grid>
+      <Grid item xs={12} md={6}>
         <ControlledSelect
           formControl={formControl}
           name="language"
