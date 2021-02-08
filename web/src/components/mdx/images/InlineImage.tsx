8b6e47ab579d80e0eec3269ba304ed3c45e57953
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-02-08 20:44:49 +0200

    Add position parameter to InlineImage

--- web/src/components/mdx/images/InlineImage.tsx
+++ web/src/components/mdx/images/InlineImage.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { makeStyles, Theme } from '@material-ui/core/styles';
-import { string } from 'prop-types';
+import { oneOf, string } from 'prop-types';
 import { useMediaQuery } from '@material-ui/core';
 import FluidImage from './FluidImage';
 import FullscreenImage from './FullscreenImage';
+
+type StyleProps = {
+  position: string
+}
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     backgroundColor: theme.palette.primary.main,
     padding: '2rem 0',
+    display: 'flex',
+    flexDirection: ({ position }: StyleProps) => (position === 'right' ? 'row-reverse' : 'row'),
+    justifyContent: ({ position }:StyleProps) => (position === 'center' ? 'center' : 'flex-start'),
     [theme.breakpoints.up('md')]: {
       padding: '2rem',
     },
@@ -24,8 +31,11 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
-const InlineImage = ({ src }) => {
-  const classes = useStyles();
+/**
+ * Standard image component for use amidst other content.
+ */
+const InlineImage = ({ src, position }) => {
+  const classes = useStyles({ position });
   const mobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
   const Image = <FluidImage src={src} className={classes.img} />;
 
@@ -39,7 +49,18 @@ const InlineImage = ({ src }) => {
 };
 
 InlineImage.propTypes = {
+  /**
+   * Name of the image to be shown.
+   */
   src: string.isRequired,
+  /**
+   * Image position (`left | center | right`).
+   */
+  position: oneOf(['left', 'center', 'right']),
+};
+
+InlineImage.defaultProps = {
+  position: 'left',
 };
 
 export default InlineImage;
