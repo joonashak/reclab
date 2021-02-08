ef1ecc18ae3f7a4532b16c0fa060ad6dc7090226
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-02-08 02:13:41 +0200

    Fix fullscreen image size and scaling

--- web/src/components/mdx/images/FullscreenImage.tsx
+++ web/src/components/mdx/images/FullscreenImage.tsx
@@ -2,18 +2,54 @@ import React, { useState } from 'react';
 import { ButtonBase, Modal } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { node, string } from 'prop-types';
+import { graphql, useStaticQuery } from 'gatsby';
 import FluidImage from './FluidImage';
+
+type StyleProps = {
+  aspectRatio: number
+}
 
 const useStyles = makeStyles({
   container: {
-    margin: '2rem',
+    margin: '2rem auto',
+    width: 'fit-content',
+  },
+  image: {
+    width: ({ aspectRatio }: StyleProps) => `calc(${aspectRatio} * (100vh - 4rem))`,
+    maxWidth: 'calc(100vw - 4rem)',
   },
 });
 
+const query = graphql`
+  query {
+    allFile {
+      nodes {
+        childImageSharp {
+          fluid {
+            originalName
+            aspectRatio
+          }
+        }
+      }
+    }
+  }
+`;
+
 const FullscreenImage = ({ trigger, src }) => {
-  const classes = useStyles();
   const [open, setOpen] = useState(false);
   const toggle = () => setOpen((prev) => !prev);
+
+  const allImages = useStaticQuery(query);
+  const image = allImages.allFile.nodes.find(
+    (img) => img.childImageSharp.fluid.originalName === src,
+  );
+
+  const { aspectRatio } = image.childImageSharp.fluid;
+  const classes = useStyles({ aspectRatio });
+
+  if (!image) {
+    return trigger;
+  }
 
   return (
     <>
@@ -22,7 +58,7 @@ const FullscreenImage = ({ trigger, src }) => {
       </ButtonBase>
       <Modal open={open} onClose={toggle}>
         <div className={classes.container}>
-          <FluidImage src={src} />
+          <FluidImage src={src} className={classes.image} />
         </div>
       </Modal>
     </>
