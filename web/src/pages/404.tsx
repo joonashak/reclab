12d2ac925f6bdbdce0599d96fa2a19eb88ab2124
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-02-09 20:28:03 +0200

    Once again, I am sourcing the frontpage from CMS
    Maybe one day, I'll make up my mind.

--- web/src/pages/404.tsx
+++ web/src/pages/404.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import LayoutWide from '../components/LayoutWide';
 import Headings from '../components/mdx/Headings';
 
 export default () => (
-  <LayoutWide>
+  <>
     <Headings.H2>Not found!</Headings.H2>
-  </LayoutWide>
+  </>
 );
