31f03424fa0757279c65cde39d05844daed4dc44
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-07-30 13:38:44 +0300

    Respect language selection in frontpage link

--- web/src/components/common/logo/LogoLinkWrapper.tsx
+++ web/src/components/common/logo/LogoLinkWrapper.tsx
@@ -1,12 +1,18 @@
 import { Link } from 'gatsby-theme-material-ui';
 import React from 'react';
 import { node, string } from 'prop-types';
+import { useTranslation } from 'react-i18next';
 
-const LogoLinkWrapper = ({ children, className }) => (
-  <Link to="/" className={className}>
-    {children}
-  </Link>
-);
+const LogoLinkWrapper = ({ children, className }) => {
+  const { i18n: { language } } = useTranslation();
+  const localizedLink = language === 'fi' ? '/fi' : '/';
+
+  return (
+    <Link to={localizedLink} className={className}>
+      {children}
+    </Link>
+  );
+};
 
 LogoLinkWrapper.propTypes = {
   children: node.isRequired,
